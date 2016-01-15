a5f218612746ea6708abac9405a083d78e4aeebe
Mark <mark@steadman.io>
2016-01-15 22:01:34 +0000

    Atomic requests

--- overspill/settings/base.py
+++ overspill/settings/base.py
@@ -22,7 +22,8 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT')
+        'PORT': os.getenv('DATABASE_PORT'),
+        'ATOMIC_REQUESTS': True
     }
 }
 
